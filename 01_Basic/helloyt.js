03829fea82fe4a2637861b0be2bf99d9120c5cf3
shivarajloni <shivarajbloni@gmail.com>
2022-06-21 13:40:41 +0530

    basic Js

---
+++ 01_Basic/helloyt.js
@@ -0,0 +1,55 @@
+console.log("Hello Shivaraj!");
+
+// variable declaration
+// var testMeVar   NOT A RECOMMENDED WAY TO DECLARE VARIABLES                // don't use var 
+ 
+// const
+
+let username = "shivaraj loni";   
+
+let userMobile = 8792248078
+let isLoggedIn = true
+
+let testMeLet
+var testMeVar
+
+console.log(testMeLet);
+console.log(testMeVar);
+
+
+
+
+Lexical structure: Elementary rules for language specification
+
+1. Texts:    JS is case-sensitive
+
+var yourMobileNumber   // your_mobile_number
+var simley = "\u"
+
+2. COMMENTS: '//', '/* anything inside is comment */',
+
+
+3. LITERALS: Number, float, string- "", '', BOOLEAN: true, false, NULL, Undefined 
+
+4. RESERVED KEYWORD: function, if, setTimeout={}, let, new, return, enum, implements, interface, package, private, protected, public, 
+
+5. UNICODE: 
+
+6. SEMICOLONS: 
+        console.log("Hey")
+        let gameScore 
+        = 
+        289;
+        console.log(gameScore);   // prints 289
+
+        return 
+        true;                   // DONT USE LIKE it actually means, return; true;
+
+        let score = 0
+        ; [score, score+3, score+5].forEach(console.log)            // defensive approach of writting code!!
+        
+        
+    
+
+        is JS compiled or interpreted language??
+            => 
